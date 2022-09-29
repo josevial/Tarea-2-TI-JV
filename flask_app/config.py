b1005650288048a792e89387ce56c3e09bd13bcc
JOSEFINA VIAL <josevial@uc.cl>
2022-09-28 22:39:04 -0300

    heroku

--- flask_app/config.py
+++ flask_app/config.py
@@ -3,7 +3,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://tstsrcmspltysg:064ec7b99640826fb85e77f08d7daa850d36a514ac0d70311328caa7c1d9e63f@ec2-35-170-146-54.compute-1.amazonaws.com:5432/deah8ikg1f54rj'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://wyzkrrlxmuynko:c9c27c1c2badb39df09d20b0cc6f8e1f050c7738d10bb599be52e010704504da@ec2-35-170-146-54.compute-1.amazonaws.com:5432/d4ebh3pjev8hq0'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
